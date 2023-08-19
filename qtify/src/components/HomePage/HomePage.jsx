b71678e32f33449661ce6fef40c89c448d584833
Uday Goel <udaygoel2000@gmail.com>
2023-08-19 21:51:22 +0530

    Added Grid using CSS

--- qtify/src/components/HomePage/HomePage.jsx
+++ qtify/src/components/HomePage/HomePage.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import HeroImage from "../HeroImage/HeroImage";
-import SongCard from "../Card/SongCard";
-import songImage from "../../assets/song.png";
 import useFetch from "../../hooks/useFetch";
 import Section from "../Section/Section";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   let backendUrl = "https://qtify-backend-labs.crio.do";
-  let cardObject = {
-    id: "1",
-    title: "New Bollywood",
-    slug: "New Bollywood",
-    image: songImage,
-    follows: "100",
-    songs: [1, 2, 3, 4, 5, 6],
-  };
 
   // let url = {
   //   topSongs: "https://qtify-backend-labs.crio.do/albums/top",
@@ -41,9 +32,10 @@ const HomePage = () => {
     <>
       <NavBar />
       <HeroImage />
-      <Section title={"Top Albums"} type="album" data={topSongsData} />
-      <Section title={"New Albums"} type="album" data={newSongsData} />
-      <SongCard cardDetails={cardObject} type={"album"} />
+      <div className={styles.sectionWrapper}>
+        <Section title={"Top Albums"} type="album" data={topSongsData} />
+        <Section title={"New Albums"} type="album" data={newSongsData} />
+      </div>
     </>
   );
 };
--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import SongCard from "../Card/SongCard";
-import { Grid } from "@mui/material";
+import { CircularProgress } from "@mui/material";
+import styles from "./Section.module.css";
 
 const Section = ({ title, type, data }) => {
+  const [carouselToggle, setCarouselToggle] = useState(false);
+  const handleToggle = () => {
+    setCarouselToggle(!carouselToggle);
+  };
   return (
     <section>
-      <div>
-        <h2>{title}</h2>
-        <Grid container spacing={0.2}>
-          {data.map((ele) => (
-            <Grid item xs={12} sm={6} md={3} key={ele.id}>
-              <SongCard cardDetails={ele} type={type} />
-            </Grid>
-          ))}
-        </Grid>
-        {/* {data.map((ele) => (
-          <SongCard cardDetails={ele} type={type} key={ele.id} />
-        ))} */}
+      <div className={styles.header}>
+        <h3>{title}</h3>
+        <h4 className={styles.toggleText} onClick={handleToggle}>
+          {carouselToggle ? "Show All" : "Collapse All"}
+        </h4>
       </div>
+      {data?.length ? (
+        <div className={styles.cardWrapper}>
+          {!carouselToggle ? (
+            <div className={styles.wrapper}>
+              {data.map((ele) => (
+                <SongCard cardDetails={ele} type={type} key={ele.id} />
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      ) : (
+        <CircularProgress />
+      )}
     </section>
   );
 };
