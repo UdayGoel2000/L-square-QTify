4e12666daea8c4c300a56fcce8e6ff6213167ca7
Uday Goel <udaygoel2000@gmail.com>
2023-08-23 08:15:35 +0530

    added transitions in accordian

--- qtify/src/components/FaqSection/FaqSection.jsx
+++ qtify/src/components/FaqSection/FaqSection.jsx
@@ -4,45 +4,87 @@ import { ReactComponent as AccordianArrow } from "../../assets/accordianArrow.sv
 
 const FaqSection = () => {
   return (
+    // <div className={styles.mainFaqDiv}>
+    //   <p className={styles.header}>FAQs</p>
+    //   <div className={styles.faqSection}>
+    //     <label htmlFor="first" className={styles.faqSectionTitle}>
+    //       <p>Is QTify free to use?</p>
+    //       <span className={styles.accordianArrowIcon}>
+    //         <AccordianArrow />
+    //       </span>
+    //     </label>
+    //     <input
+    //       type="checkbox"
+    //       name="accordian"
+    //       className="accordian-toggle
+    //     "
+    //       id="first"
+    //     />
+    //     <div className={styles.faqSectionDetails}>
+    //       <p>Yes! It is 100% free, and has 0% ads!</p>
+    //     </div>
+    //   </div>
+    //   <div className={styles.faqSection}>
+    //     <label htmlFor="second" className={styles.faqSectionTitle}>
+    //       <p>Can I download and listen to songs offline?</p>
+    //       <span className={styles.accordianArrowIcon}>
+    //         <AccordianArrow />
+    //       </span>
+    //     </label>
+    //     <input
+    //       type="checkbox"
+    //       name="accordian"
+    //       className="accordian-toggle
+    //     "
+    //       id="second"
+    //     />
+    //     <div className={styles.faqSectionDetails}>
+    //       <p>
+    //         Sorry, unfortunately we don't provide the service to download any
+    //         songs.
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
     <div className={styles.mainFaqDiv}>
       <p className={styles.header}>FAQs</p>
-      <div className={styles.faqSection}>
-        <label htmlFor="first" className={styles.faqSectionTitle}>
-          <p>Is QTify free to use?</p>
-          <span className={styles.accordianArrowIcon}>
-            <AccordianArrow />
-          </span>
-        </label>
-        <input
-          type="checkbox"
-          name="accordian"
-          className="accordian-toggle
-        "
-          id="first"
-        />
-        <div className={styles.faqSectionDetails}>
-          <p>Yes! It is 100% free, and has 0% ads!</p>
+      <div className={styles.accordian}>
+        <div className={styles.accordianTab}>
+          <input
+            type="checkbox"
+            id="first"
+            className={styles.accordianToggle}
+            name="toggle"
+          />
+          <label className={styles.accordianTitle} htmlFor="first">
+            Is QTify free to use?
+            <span className={styles.accordianArrowIcon}>
+              <AccordianArrow />
+            </span>
+          </label>
+          <div className={styles.accordianContent}>
+            <p>Yes! It is 100% free, and has 0% ads!</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.faqSection}>
-        <label htmlFor="second" className={styles.faqSectionTitle}>
-          <p>Can I download and listen to songs offline?</p>
-          <span className={styles.accordianArrowIcon}>
-            <AccordianArrow />
-          </span>
-        </label>
-        <input
-          type="checkbox"
-          name="accordian"
-          className="accordian-toggle
-        "
-          id="second"
-        />
-        <div className={styles.faqSectionDetails}>
-          <p>
-            Sorry, unfortunately we don't provide the service to download any
-            songs.
-          </p>
+        <div className={styles.accordianTab}>
+          <input
+            type="checkbox"
+            id="second"
+            className={styles.accordianToggle}
+            name="toggle"
+          />
+          <label className={styles.accordianTitle} htmlFor="second">
+            Can I download and listen to songs offline?
+            <span className={styles.accordianArrowIcon}>
+              <AccordianArrow />
+            </span>
+          </label>
+          <div className={styles.accordianContent}>
+            <p>
+              Sorry, unfortunately we don't provide the service to download any
+              songs.
+            </p>
+          </div>
         </div>
       </div>
     </div>
